5e55b593ee02016c839abdef72e957432a25c61f
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2025-08-18 20:43:09 -0300

    1er commit

---
+++ src/test/java/com/curso_modulos_spring/best_travel/BestTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_modulos_spring.best_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
